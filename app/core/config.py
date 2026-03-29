0c385af99780576c6dcea05354a01d7b9b472527
Niranjan G <67407690+NiranjanG8@users.noreply.github.com>
2026-03-30 00:30:34 +0530

    Create config.py

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ASSISTANT_NAME = "aira"
+
+# STT mode: auto / offline / online
+LISTEN_MODE = "auto"
+
+# Paths
+VOSK_MODEL_PATH = "models/vosk-model-small-en-us-0.15"
+
+# Piper TTS
+PIPER_PATH = "tts/piper/piper.exe"
+PIPER_MODEL = "tts/piper/en_US-lessac-medium.onnx"
