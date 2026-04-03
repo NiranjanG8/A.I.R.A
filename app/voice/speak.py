c6894114d65d05a3931c510c172b3f7ffaae06e5
Niranjan G <67407690+NiranjanG8@users.noreply.github.com>
2026-04-03 17:09:53 +0530

    Update speak.py

--- app/voice/speak.py
+++ app/voice/speak.py
@@ -1,17 +1,19 @@
-import subprocess
-from app.core.config import PIPER_PATH, PIPER_MODEL
+from piper import PiperVoice
+import sounddevice as sd
+import numpy as np
+
+# Load voice once
+voice = PiperVoice.load("tts/piper/en_US-lessac-medium.onnx")
 
 def speak(text: str):
     print("Aira:", text)
 
     try:
-        subprocess.run(
-            [
-                PIPER_PATH,
-                "--model", PIPER_MODEL,
-                "--output-raw"
-            ],
-            input=text.encode("utf-8")
-        )
+        audio = b"".join(voice.synthesize(text))
+
+        audio_np = np.frombuffer(audio, dtype=np.int16)
+        sd.play(audio_np, samplerate=22050)
+        sd.wait()
+
     except Exception as e:
         print("TTS error:", e)
